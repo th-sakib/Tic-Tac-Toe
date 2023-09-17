76c0d2ec97a31947a5c80382029faf32f1097505
sakib <mdthamidulhasansakib@gmail.com>
2023-09-17 22:28:17 +0600

    turn changing bug fixed

--- custom.js
+++ custom.js
@@ -14,10 +14,10 @@ const turnChange = () => {
 }
 const isWin = () => {
     const winCons = [
-        [0, 1, 2, 2.5, 5.3, 0],// last 3 translate X, translate Y, rotatevw, 28.7vw
-        [3, 4, 5, 2.5, 17, 0],
-        [6, 7, 8, 2.5, 28.7, 0],
-        [0, 3, 6, ],
+        [0, 1, 2],// last 3 translate X, translate Y, rotatevw, 28.7vw
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
@@ -26,8 +26,12 @@ const isWin = () => {
 
     winCons.forEach(con => {
         if ((boxes[con[0]].innerText == boxes[con[1]].innerText) && (boxes[con[2]].innerText == boxes[con[1]].innerText) && (boxes[con[0]].innerText != '')) {
-            document.getElementById('turn').innerText = `${boxes[con[0]].innerText} Won`
             isVictory = true;
+            if (isVictory) {
+                document.getElementById('turn').innerText = `${boxes[con[0]].innerText} Won`;
+                
+                isVictory = false;
+            }
             document.querySelector('.vanish').style.opacity = 1;
         }
     })
@@ -58,7 +62,7 @@ reset.addEventListener('click', function () {
         box.innerText = '';
         document.querySelector('.vanish').style.opacity = 0;
         document.getElementById('turn').innerText = `Turn for: X`;
-
+        turn = "X";
     })
 })
 // switch between theme button handle
